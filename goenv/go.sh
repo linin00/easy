7dd7d8d5f8fea217aaa4d67363667667329ac383
linin00 <lianjie35@outlook.com>
2022-04-27 23:55:42 +0800

    update

--- goenv/go.sh
+++ goenv/go.sh
@@ -1,11 +1,11 @@
 wget https://go.dev/dl/go1.18.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzvf go1.18.linux-amd64.tar.gz
-sudo mkdir GOPATH
-sudo mkdir GOPATH/bin
-sudo mkdir GOPATH/src
-sudo mkdir GOPATH/pkg
+sudo mkdir ~/.GOPATH
+sudo mkdir ~/.GOPATH/bin
+sudo mkdir ~/.GOPATH/src
+sudo mkdir ~/.GOPATH/pkg
 cat >> ~/.zshrc << EOF
-export GOPATH=/GOPATH
+export GOPATH=~/.GOPATH
 export GOROOT=/usr/local/go
 export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 EOF
